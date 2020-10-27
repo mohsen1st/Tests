cc7f326763ec9cc7e43ea0580f48c51fcffda3e5
Mohsen Salehi <mohsen1st@live.com>
2020-10-27 23:40:34 +1100

    pet name adoptions corrected

--- pets/adoptions/views.py
+++ pets/adoptions/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from django.http import Http404
+from .models import Pet
 def home(request):
-    return HttpResponse('<p>home view</p>')
-
+    pets = Pet.objects.all()
+    return render(request,'home.html',{
+        'pets': pets,
+    }) #using a template, render function uses a dictionary (we use pets here)
+#we have created a templates folder and named the files "home.html"
+#and "pet_detail.html"
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with id {pert_id} </p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html',{
+        'pet': pet,
+    })
--- pets/pets/urls.py
+++ pets/pets/urls.py
@@ -6,5 +6,5 @@ from adoptions import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name= 'home'),
-    path('adaptions/<int:pet_id>/',views.pet_detail, name='pet_detail')
+    path('adoptions/<int:pet_id>/',views.pet_detail, name='pet_detail')
 ]
